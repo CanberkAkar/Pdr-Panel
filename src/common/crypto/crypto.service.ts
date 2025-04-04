7b05cc6079d0cf74ea70924ba26dde36e0e29738
CanberkAkar <akarcanberk@gmail.com>
2025-04-04 16:20:56 +0300

    Updated cyrpto service

--- src/common/crypto/crypto.service.ts
+++ src/common/crypto/crypto.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as CryptoJS from 'crypto-js';
-
+import { createHash } from 'crypto';
 @Injectable()
 export class CryptoService {
   private secretKey = process.env.SECRETKEY;
@@ -12,5 +12,10 @@ export class CryptoService {
   decrypt(encryptedData: string): any {
     const bytes = CryptoJS.AES.decrypt(encryptedData, this.secretKey);
     return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  }
+  hashPassword(password: string): string {
+    const hash = createHash('md5');
+    hash.update(password);
+    return hash.digest('hex');  // Şifreyi hex formatında döner
   }
 }
