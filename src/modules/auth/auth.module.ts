6b4ca1d429a0a7659280b8ac90c53e602b9eed7e
CanberkAkar <akarcanberk@gmail.com>
2025-04-21 16:23:53 +0300

    Updated auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,16 +6,23 @@ import { Users } from './entites/auth.entity';
 import { CryptoModule } from './../../common/crypto/crypto.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './auth.strategy'; // bunu birazdan oluşturacağız
-import { UserToken } from './entites/token.entity'; // token entity'yi içe aktar
+import { JwtStrategy } from './auth.strategy';
+import { UserToken } from './entites/token.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Users,UserToken]),
+    ConfigModule, // Bu satırı unutma
+    TypeOrmModule.forFeature([Users, UserToken]),
     CryptoModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'test',  
-      signOptions: { expiresIn: '1h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRETKEY'),
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
   ],
   controllers: [AuthController],
