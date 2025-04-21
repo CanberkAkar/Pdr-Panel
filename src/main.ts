a139330b9400b93e872b27170bd9d012e79fc0b4
CanberkAkar <akarcanberk@gmail.com>
2025-04-21 16:22:55 +0300

    Updated main ts

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,21 @@ async function bootstrap() {
     .setTitle('PDR API')
     .setDescription('API documentation')
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'access-token',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('', app, document); 
+
   await app.listen(3000);
 }
 bootstrap();
