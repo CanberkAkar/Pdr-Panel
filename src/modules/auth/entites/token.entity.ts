83309b2ac2b8f453619207efe8ce0c1251bf9545
CanberkAkar <akarcanberk@gmail.com>
2025-04-21 12:48:17 +0300

    Updated token entity

--- src/modules/auth/entites/token.entity.ts
+++ src/modules/auth/entites/token.entity.ts
@@ -1,16 +1,19 @@
-import { Column, CreateDateColumn, Entity ,PrimaryGeneratedColumn} from "typeorm";
-
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('user_tokens')
 export class UserToken {
-    //DB MODELINI OLUŞTURUYORUZ
-    
-    @PrimaryGeneratedColumn()
-    id: number;
-    @Column()
-    userId: number;
-    @Column()
-    token:string;
-    @CreateDateColumn()
-    createDate!:Date;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  user_id: number;
+
+  @Column()
+  token: string;
+
+  @CreateDateColumn()
+  create_date: Date;
+
+  @Column({ type: 'timestamp' })
+  expires_at: Date;
 }
