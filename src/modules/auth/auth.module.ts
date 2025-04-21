0b25d428f331c2bb90c0da8c8064944ae63eee99
CanberkAkar <akarcanberk@gmail.com>
2025-04-21 12:47:29 +0300

    Updated auth modules

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,14 +4,21 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entites/auth.entity';
 import { CryptoModule } from './../../common/crypto/crypto.module';
-
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './auth.strategy'; // bunu birazdan oluşturacağız
+import { UserToken } from './entites/token.entity'; // token entity'yi içe aktar
 @Module({
-  imports:[
-    TypeOrmModule.forFeature([Users]),
-    CryptoModule
+  imports: [
+    TypeOrmModule.forFeature([Users,UserToken]),
+    CryptoModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'test',  
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
- 
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
