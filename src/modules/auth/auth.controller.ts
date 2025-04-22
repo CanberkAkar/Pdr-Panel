26be8113918b03361d7727e8c3d384ceeaf6fade
CanberkAkar <akarcanberk@gmail.com>
2025-04-22 17:47:19 +0300

    Updated auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Request, UseGuards } from '@nestjs/common'
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LoginUserDto } from './dto/login-user.dto';
-
+import { CreateUserDto } from './dto/create-user.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -12,10 +12,16 @@ export class AuthController {
     return this.authService.login(loginUserDto);
   }
 
-@UseGuards(AuthGuard('jwt')) // <--- burada koruma geldi
-  @Get('/list')
-  list(@Request() req) {
-    console.log('Giriş yapan kullanıcı:', req);
-    return this.authService.list();
+  @UseGuards(AuthGuard('jwt')) 
+    @Get('/list')
+    list(@Request() req) {
+      console.log('Giriş yapan kullanıcı:', req);
+      return this.authService.list();
+    }
+    @UseGuards(AuthGuard('jwt')) 
+    @Post('/insert')
+    insert(@Body() createUserDto: CreateUserDto) {
+      return this.authService.insert(createUserDto);
+    }
   }
-}
+ 
