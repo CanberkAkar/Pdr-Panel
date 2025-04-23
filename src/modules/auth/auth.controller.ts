318b6ef60cea6044d2cc8e779d402d59a298ca03
CanberkAkar <akarcanberk@gmail.com>
2025-04-23 16:09:40 +0300

    Updated auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Request, UseGuards,Put,Param,Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LoginUserDto } from './dto/login-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -22,6 +23,20 @@ export class AuthController {
     @Post('/insert')
     insert(@Body() createUserDto: CreateUserDto) {
       return this.authService.insert(createUserDto);
+    }
+    @UseGuards(AuthGuard('jwt'))
+    @Put('/update/:id')
+    update(
+      @Param('id') userId: number,
+      @Body() body: UpdateUserDto
+    ) {
+      body.id = Number(userId); // Doğru kullanım
+      return this.authService.update(body);
+    }
+    @UseGuards(AuthGuard('jwt'))
+    @Delete('/delete/:id')
+    deleteUser(@Param('id') userId: number) {
+      return this.authService.delete(userId);
     }
   }
  
