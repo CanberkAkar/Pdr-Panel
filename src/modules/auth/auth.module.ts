8306f42574bfc8a5506e0fe6bb19eef45f62e724
CanberkAkar <akarcanberk@gmail.com>
2025-04-24 17:41:24 +0300

    Updated auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Users } from './entites/auth.entity';
+import { Users } from './entities/auth.entity';
 import { CryptoModule } from './../../common/crypto/crypto.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from '../../config/auth.strategy';
-import { UserToken } from './entites/token.entity';
+import { UserToken } from './entities/token.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
