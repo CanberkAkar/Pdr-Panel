1b2379cc284ced39cce3aaee6ed0d312b927b362
CanberkAkar <akarcanberk@gmail.com>
2025-04-24 17:27:48 +0300

    Updated main ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+//addBearerAuth kullanırsak tokenin önünde Bearer keyini de gönderecektir. addApiKey kullanırsak sadece tokeni gönderecektir.
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('PDR API')
-    .setDescription('API documentation')
-    .setVersion('1.0')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'Authorization',
-        in: 'header',
-      },
-      'access-token',
-    )
-    .build();
+  .setTitle('PDR API')
+  .setDescription('API documentation')
+  .setVersion('1.0')
+  .addApiKey(
+    {
+      type: 'apiKey',
+      name: 'Authorization',
+      in: 'header',
+    },
+    'access-token',
+  )
+  .build();
+
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('', app, document); 
