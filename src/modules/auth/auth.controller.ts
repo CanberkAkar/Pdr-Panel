fb4dd5fa7108f2ff0847c302f53840ed7d370683
CanberkAkar <akarcanberk@gmail.com>
2025-04-24 17:28:00 +0300

    Updated auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, Post, Body, Request, UseGuards,Put,Param,Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Request, UseGuards, Put, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LoginUserDto } from './dto/login-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('auth') // Swagger UI'da 'auth' grubu
+@ApiBearerAuth('access-token') // Buradaki 'access-token', main.ts'deki key ile aynı olmalı
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -13,30 +17,36 @@ export class AuthController {
     return this.authService.login(loginUserDto);
   }
 
-  @UseGuards(AuthGuard('jwt')) 
-    @Get('/list')
-    list(@Request() req) {
-      console.log('Giriş yapan kullanıcı:', req);
-      return this.authService.list();
-    }
-    @UseGuards(AuthGuard('jwt')) 
-    @Post('/insert')
-    insert(@Body() createUserDto: CreateUserDto) {
-      return this.authService.insert(createUserDto);
-    }
-    @UseGuards(AuthGuard('jwt'))
-    @Put('/update/:id')
-    update(
-      @Param('id') userId: number,
-      @Body() body: UpdateUserDto
-    ) {
-      body.id = Number(userId); // Doğru kullanım
-      return this.authService.update(body);
-    }
-    @UseGuards(AuthGuard('jwt'))
-    @Delete('/delete/:id')
-    deleteUser(@Param('id') userId: number) {
-      return this.authService.delete(userId);
-    }
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/list')
+  list(@Request() req) {
+    return this.authService.list();
   }
- 
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('/insert')
+  insert(@Body() createUserDto: CreateUserDto) {
+    return this.authService.insert(createUserDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put('/update/:id')
+  update(@Param('id') userId: number, @Body() body: UpdateUserDto) {
+    body.id = Number(userId);
+    return this.authService.update(body);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('/delete/:id')
+  deleteUser(@Param('id') userId: number) {
+    return this.authService.delete(userId);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('/logout')
+  logout(@Request() req) {
+    const authHeader = req.headers['authorization'] || '';
+    const token = authHeader.replace('Bearer ', '').trim();
+    return this.authService.logout(token);
+  }
+}
