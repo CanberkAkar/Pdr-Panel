a3e742eb0ed2789e404534cb6c3f308aa1376099
CanberkAkar <akarcanberk@gmail.com>
2025-05-02 14:19:52 +0300

    Updated appointments controller test

--- src/modules/appointments/appointments.controller.spec.ts
+++ src/modules/appointments/appointments.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppointmentsController } from './appointments.controller';
-
+import { AppointmentsService } from './appointments.service';
+import { CreateAppointmentDto } from './dto/create-appointment-dto';
 describe('AppointmentsController', () => {
   let controller: AppointmentsController;
-
+  let mockAppointmentService: Partial<AppointmentsService>;
+ 
   beforeEach(async () => {
+    mockAppointmentService = {
+      list: jest.fn().mockReturnValue('User list')
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AppointmentsController],
+      providers: [
+        {
+          provide: AppointmentsService,
+          useValue: mockAppointmentService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AppointmentsController>(AppointmentsController);
@@ -14,5 +26,9 @@ describe('AppointmentsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should return appointment list', () => {
+    const result = controller.list({});
+    expect(result).toBe('User list');
   });
 });
