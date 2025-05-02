e0b36c470bf0c382387811bb37f2651e2a457b69
CanberkAkar <akarcanberk@gmail.com>
2025-05-02 14:23:41 +0300

    Updated appointments controller test

--- src/modules/appointments/appointments.controller.spec.ts
+++ src/modules/appointments/appointments.controller.spec.ts
@@ -8,8 +8,9 @@ describe('AppointmentsController', () => {
  
   beforeEach(async () => {
     mockAppointmentService = {
-      list: jest.fn().mockReturnValue('User list')
-    };
+      list: jest.fn().mockReturnValue('Appointment list'),
+      insert: jest.fn().mockReturnValue('Appointment insert')
+     };
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AppointmentsController],
@@ -29,6 +30,11 @@ describe('AppointmentsController', () => {
   });
   it('should return appointment list', () => {
     const result = controller.list({});
-    expect(result).toBe('User list');
+    expect(result).toBe('Appointment list');
   });
+  it('should return appointment insert', ()=>{
+    const result = controller.insert({} as CreateAppointmentDto);
+    expect(result).toBe('Appointment insert');
+  });
+
 });
