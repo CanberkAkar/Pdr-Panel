390064a282e8058e5aff6d2499eda3ccb18f084b
CanberkAkar <akarcanberk@gmail.com>
2025-05-05 15:13:44 +0300

    Updated appointment controller test

--- src/modules/appointments/appointments.controller.spec.ts
+++ src/modules/appointments/appointments.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppointmentsController } from './appointments.controller';
 import { AppointmentsService } from './appointments.service';
 import { CreateAppointmentDto } from './dto/create-appointment-dto';
+import { UpdateAppointmentDto } from './dto/update-appointment-dto';
 describe('AppointmentsController', () => {
   let controller: AppointmentsController;
   let mockAppointmentService: Partial<AppointmentsService>;
@@ -9,7 +10,9 @@ describe('AppointmentsController', () => {
   beforeEach(async () => {
     mockAppointmentService = {
       list: jest.fn().mockReturnValue('Appointment list'),
-      insert: jest.fn().mockReturnValue('Appointment insert')
+      insert: jest.fn().mockReturnValue('Appointment insert'),
+      update: jest.fn().mockReturnValue('Appointment update')
+
      };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -36,5 +39,10 @@ describe('AppointmentsController', () => {
     const result = controller.insert({} as CreateAppointmentDto);
     expect(result).toBe('Appointment insert');
   });
+  it('should return appointment update', ()=>{
+    const result = controller.update(1, {} as UpdateAppointmentDto);
+    expect(result).toBe('Appointment update');
+  });
+
 
 });
