4fc446453693edffad6af4f288e3e5fe055d32f5
CanberkAkar <akarcanberk@gmail.com>
2025-05-06 10:52:18 +0300

    Updated appointment service

--- src/modules/appointments/appointments.service.ts
+++ src/modules/appointments/appointments.service.ts
@@ -4,8 +4,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Appointments } from './entities/appointments.entity';
 import { UserToken } from '../auth/entities/token.entity';
 import { CryptoService } from '../../common/crypto/crypto.service';
-import { JwtService } from '@nestjs/jwt';
 import { CreateAppointmentDto } from './dto/create-appointment-dto';
+import { UpdateAppointmentDto } from './dto/update-appointment-dto';
 @Injectable()
 export class AppointmentsService {
     private readonly logger = new Logger(AppointmentsService.name);
@@ -13,9 +13,7 @@ export class AppointmentsService {
     constructor(
         @InjectRepository(Appointments) private readonly appointmentRepository: Repository<Appointments>,
         @InjectRepository(UserToken)
-        private readonly tokenRepository: Repository<UserToken>,
-        private readonly jwtService: JwtService,
-        private readonly cyrptoService: CryptoService,
+          private readonly cyrptoService: CryptoService,
       ) {}
       async list() {
         await this.logger.log('Attempting to list all users');
@@ -24,6 +22,25 @@ export class AppointmentsService {
         return { status: "200", appointments: appointments };
        }
       async insert(createAppointmentDto: CreateAppointmentDto) {
-      
+        this.logger.log('Attempting to create a new user');
+        try {
+          const appointment = this.appointmentRepository.create(createAppointmentDto);
+          appointment.psychologistId = createAppointmentDto.psychologistId;
+          appointment.patientName = createAppointmentDto.patientName;
+          appointment.patientId = createAppointmentDto.patientId;
+          appointment.duration = createAppointmentDto.duration;
+          appointment.status = createAppointmentDto.status;
+          appointment.notes = createAppointmentDto.notes;
+          await this.appointmentRepository.save(appointment);
+    
+          this.logger.log(`Appointment created successfully with ID: ${appointment.id}`);
+          return this.cyrptoService.encrypt({ status: '201', appointment: appointment });
+        } catch (error) {
+          return this.cyrptoService.encrypt({
+            status: '400',
+            message: 'User not created',
+          });
+        }
       }
+      async update(updateAppointmentDto:UpdateAppointmentDto){}
 }
