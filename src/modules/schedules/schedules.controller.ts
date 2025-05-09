0c289c113ad141151be83e38ea3ae1d38f1d82ed
CanberkAkar <akarcanberk@gmail.com>
2025-05-09 17:52:26 +0300

    Updated schedulers controller

--- src/modules/schedules/schedules.controller.ts
+++ src/modules/schedules/schedules.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Request, UseGuards, Put, Param, Delete } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { SchedulesService } from './schedules.service';
 
+@ApiBearerAuth('access-token')
 @Controller('schedules')
-export class SchedulesController {}
+
+export class SchedulesController {
+    constructor(private readonly schedulesService:SchedulesService) {}
+    @UseGuards(AuthGuard('jwt'))
+    @Get('/list')
+    list(@Request() req) {
+      return this.schedulesService.list();
+    }
+}
