fc006cbd6db576228dafcd830f08b31813fe496a
CanberkAkar <akarcanberk@gmail.com>
2025-05-12 16:31:56 +0300

    Updated schedules controller ts

--- src/modules/schedules/schedules.controller.ts
+++ src/modules/schedules/schedules.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Post, Body, Request, UseGuards, Put, Param, Delete } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { SchedulesService } from './schedules.service';
+import { CreateScheduleDto } from './dto/create-schedules-dto';
+import { UpdateScheduleDto } from './dto/update-schedules-dto';
 
+@ApiTags('Schedules') // Swagger UI'da 'auth' grubu
 @ApiBearerAuth('access-token')
 @Controller('schedules')
 
@@ -12,5 +15,16 @@ export class SchedulesController {
     @Get('/list')
     list(@Request() req) {
       return this.schedulesService.list();
+    }
+    @UseGuards(AuthGuard('jwt'))
+    @Post('/insert')
+    insert(@Body() createScheduleDto:CreateScheduleDto) {
+      return this.schedulesService.insert(createScheduleDto);
+    }
+    @UseGuards(AuthGuard('jwt'))
+    @Put('/update/:id')
+    update(@Param('id') scheduleId: number, @Body() body: UpdateScheduleDto) {
+      body.id = Number(scheduleId);
+      return this.schedulesService.update(body);
     }
 }
